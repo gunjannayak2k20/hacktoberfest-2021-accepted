97560c53175802583c47b08345b077ed2992a82c
Kanishk Harde <66664028+kanishkharde@users.noreply.github.com>
2021-10-12 11:37:32 +0530

    Create Stack.py

---
+++ DSA in python/Stack.py
@@ -0,0 +1,20 @@
+stack = []
+ 
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+ 
+print('Initial stack')
+print(stack)
+ 
+# pop() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+ 
+print('\nStack after elements are popped:')
+print(stack)
