e303fc7c1acad46a821cb43e18b36c5475683c9f
ganeshmajhi <ganeshmajhi@gmail.com>
2021-10-16 14:02:50 +0530

    added a while loop example

---
+++ DSA in python/DoWhile.py
@@ -0,0 +1,10 @@
+# prints all letters except 'a' and 's'   
+i = 0  
+str1 = 'chetnaganesh'  
+  
+while i < len(str1):   
+    if str1[i] == 'a' or str1[i] == 't':   
+        i += 1  
+        continue  
+    print('Current Letter :', a[i])   
+    i += 1  
